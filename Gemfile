2098e489d8cdee7ea3cd70e94605c70adf5156f8
Athif Wulandana <athif@u.northwestern.edu>
2014-12-14 17:25:44 -0500

    Installed Devise and Heroku

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.1.0.rc2'
 
 group :development, :test do
      gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  #for heroku
+  config.action_mailer.default_url_options = { :host => 'http://athif-pinteresting-commits.herokuapp.com/' }
 end
